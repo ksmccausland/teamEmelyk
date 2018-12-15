0828f48716a9cb2bd9f7029a858e4e4e6446e5a0
Emily Johnson <emjohnson@bishopblanchet.org>
2018-12-15 12:46:39 -0800

    Adding solenoid and encoder

--- TestProject/src/main/java/frc/robot/Robot.java
+++ TestProject/src/main/java/frc/robot/Robot.java
@@ -7,19 +7,19 @@ import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.GenericHID.Hand;
 import edu.wpi.first.wpilibj.Ultrasonic;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.Encoder;
 
 public class Robot extends IterativeRobot {
   private static final int kFrontLeftChannel = 3;
   private static final int kRearLeftChannel = 2;
   private static final int kFrontRightChannel = 4;
   private static final int kRearRightChannel = 1;
-  private static final int kArmChannel = 5;
 
   private TalonSRX frontLeft;
   private TalonSRX rearLeft;
   private TalonSRX frontRight;
   private TalonSRX rearRight;
-  private TalonSRX arm;
 
   private static final int kJoystickChannel = 0;
   private XboxController joystick;
@@ -28,14 +28,27 @@ public class Robot extends IterativeRobot {
   private static final int kPingChannel = 1;
   private static final int kEchoChannel = 1;
   private Ultrasonic ultrasonic;
+  // We need to measure what this height is
+  private static final double kRaiseHeight_MM = 150; // ?
+  private static final double kLowerHeight_MM = 20; // ?
+
+  // Encoder
+  // TODO: Check sourceA and sourceB
+  private static final int kEncoderSourceA = 0;
+  private static final int kEncoderSourceB = 1;
+  private Encoder armEncoder;
+  // We need figure out this value?
+  private static final double kHeightEncoderCount = 100; // ?
 
   // state for arm
   private static boolean currentlyLifting;
   private static boolean currentlyLowering;
 
-  // We need to measure what this height is
-  private static final double kRaiseHeight_MM = 150; // ?
-  private static final double kLowerHeight_MM = 20; // ?
+  // Pneumatics
+  // TODO: check channels
+  private static final int kSolenoidForwardChannel = 1;
+  private static final int kSolenoidReverseChannel = 2;
+  DoubleSolenoid solenoid;
 
   @Override
   public void robotInit() {
@@ -43,18 +56,25 @@ public class Robot extends IterativeRobot {
     rearLeft = new TalonSRX(kRearLeftChannel);
     frontRight = new TalonSRX(kFrontRightChannel);
     rearRight = new TalonSRX(kRearRightChannel);
-    arm = new TalonSRX(kArmChannel);
 
     // invert drive train from tank drive
     frontLeft.setInverted(true);
     rearRight.setInverted(true);
 
-    // joystick
-    joystick = new XboxController(kJoystickChannel);
+    // do we have a motor controller for the arm, or is it just pneumatics?
+    solenoid = new DoubleSolenoid(kSolenoidForwardChannel, kSolenoidReverseChannel);
 
     // ultrasonic
     ultrasonic = new Ultrasonic(kPingChannel, kEchoChannel);
     ultrasonic.setAutomaticMode(true); // Do we want the automatic pinging algorithm?
+
+    // encoder
+    // TODO: is the encoder mounted upsidedown?
+    armEncoder = new Encoder(kEncoderSourceA, kEncoderSourceB, false, Encoder.EncodingType.k4X);
+    armEncoder.reset();
+
+    // joystick
+    joystick = new XboxController(kJoystickChannel);
   }
 
   @Override
@@ -75,8 +95,6 @@ public class Robot extends IterativeRobot {
      handleArm(aButton, bButton);
     }
 
-    
-
   }
 
   private void handleArm(boolean aButton, boolean bButton) {
@@ -91,19 +109,26 @@ public class Robot extends IterativeRobot {
 
     // only turn on ultrasonic if we are lifting/lowering
     double armPower = 0;
+    DoubleSolenoid.Value solenoidValue = DoubleSolenoid.Value.kOff;
     if (currentlyLifting || currentlyLowering) {
-      double range = ultrasonic.getRangeMM();
-      if (currentlyLifting && range < kRaiseHeight_MM) {
+      double encoderCount = armEncoder.getRaw();
+      if (encoderCount < kHeightEncoderCount) {
         armPower = 1;
+      }
+
+      if (currentlyLifting) {
+        solenoidValue = DoubleSolenoid.Value.kForward;
       }
 
       if (currentlyLowering && range > kLowerHeight_MM) {
         armPower = -1;
+        solenoidValue = DoubleSolenoid.Value.kReverse;
       }
     }
 
     // move arm
-    arm.set(ControlMode.PercentOutput, armPower);
+    // TODO: may want to control power based on distance to target
+    solenoid.set(solenoidValue);
 
     // reset if no longer need power
     if (armPower == 0) {
